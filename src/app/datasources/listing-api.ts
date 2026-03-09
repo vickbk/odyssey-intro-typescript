316c7fad2b6a70e88ea1b4d966c2618a7d65b457
vickbk <vickbake8@gmail.com>
2026-03-09 13:39:22 +0200

    feat: adding createListing methode to the listing api

--- src/app/datasources/listing-api.ts
+++ src/app/datasources/listing-api.ts
@@ -1,16 +1,25 @@
 import { RESTDataSource } from "@apollo/datasource-rest";
-import { Amenity, Listing } from "../types";
+import { Amenity, CreateListingInput, Listing } from "../types";
 
 export class ListingApi extends RESTDataSource {
   baseURL = "https://rt-airlock-services-listing.herokuapp.com/";
+
   getFeaturedListings() {
     return this.get<Listing[]>("featured-listings");
   }
+
   getListing(id: string) {
     return this.get<Listing>(`listings/${id}`);
   }
+
   getAmenities(listingId: string) {
     console.log("Making a follow-up call for amenities with ", listingId);
     return this.get<Amenity[]>(`listings/${listingId}/amenities`);
+  }
+
+  createListing(listing: CreateListingInput) {
+    return this.post<Listing>("listings", {
+      body: { listing },
+    });
   }
 }
