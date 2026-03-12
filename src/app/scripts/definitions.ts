6e6f5854ef033c266c09b197d0c396521d6d41e4
vickbk <vickbake8@gmail.com>
2026-03-12 13:45:14 +0200

    feat: defining different graphql types in dedicated folders

--- src/app/scripts/definitions.ts
+++ src/app/scripts/definitions.ts
@@ -1,8 +1,12 @@
+import { amenityTypesDef, listingDefinitions } from "@/features";
 import { gqlFileLoader } from "@/shared";
+import { DocumentNode } from "graphql";
 
-export const typeDefs = [
+export const typeDefs: DocumentNode[] = [
   ...gqlFileLoader({
     folder: __dirname,
     files: ["schema.graphql"],
   }),
+  ...amenityTypesDef,
+  ...listingDefinitions,
 ];
---
+++ src/features/listing/scripts/index.ts
@@ -0,0 +1,2 @@
+export * from "./listing.definitions";
+// export * from "./listing.resolvers";
